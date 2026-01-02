23f6f51cc21ccc6e19e2c0b4e42388ee193e8d5b
samnangsamnang048 <samnangsamnang048@gmail.com>
2026-01-02 14:09:19 +0700

    home

--- lib/core/constants/app_themes.dart
+++ lib/core/constants/app_themes.dart
@@ -1,5 +1,45 @@
+import 'package:flutter/material.dart';
+
 class AppThemes {
-  static const String lightTheme = 'light';
-  static const String darkTheme = 'dark';
-  static const String systemTheme = 'system';
+  // Text themes
+  final TextTheme lightTextTheme = const TextTheme(
+    bodyMedium: TextStyle(color: Colors.black),
+    bodyLarge: TextStyle(color: Colors.black, fontSize: 24),
+    titleLarge: TextStyle(color: Colors.black),
+  );
+  final TextTheme darkTextTheme = const TextTheme(
+    bodyMedium: TextStyle(color: Colors.white),
+    bodyLarge: TextStyle(color: Colors.white, fontSize: 24),
+    titleLarge: TextStyle(color: Colors.white),
+  );
+
+  // Light theme
+  ThemeData lightTheme(BuildContext context) => ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: lightTextTheme,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.blue,
+    ),
+  );
+
+  // Dark theme
+  ThemeData darkTheme(BuildContext context) => ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.blueGrey,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: darkTextTheme,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.grey,
+      foregroundColor: Colors.white,
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Colors.blueGrey,
+    ),
+  );
 }
